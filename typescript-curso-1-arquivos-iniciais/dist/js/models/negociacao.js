598fc4f86f1bb802e06995d25d449bb039e86c1e
Caio Henrique <caiohenriqueviana01@gmail.com>
2026-02-13 11:58:51 -0300

    models

--- typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
@@ -1,24 +1,19 @@
 export class Negociacao {
-    #data;
-    #quantidade;
-    #valor
-
-    constructor(data, quantidade, valor){
-        this.#data = data;
-        this.#quantidade = quantidade;
-        this.#valor = valor;
+    constructor(_data, _quantidade, _valor) {
+        this._data = _data;
+        this._quantidade = _quantidade;
+        this._valor = _valor;
     }
-    get data(){
-        return this.#data
+    get data() {
+        return this._data;
     }
-    get quantidade(){
-        return this.#quantidade;
+    get quantidade() {
+        return this._quantidade;
     }
-    get valor(){
-        return this.#valor;
+    get valor() {
+        return this._valor;
     }
-    get volume(){
-        return this.#quantidade * this.#valor;
+    get volume() {
+        return this._quantidade * this._valor;
     }
 }
-
